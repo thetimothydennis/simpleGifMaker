8b031a87aea664a8d9c4903083976b37f454eec9
Timothy Dennis <timothyddennis@gmail.com>
2023-11-15 07:00:03 -0600

    feat: added input for framerate to change speed of output gif

--- gifMaker.sh
+++ gifMaker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "This program generates an animated gif from the images stored in the sourceImgs folder. The inputs in this script are case-sensitive.\n"
+echo -e "This program generates an animated gif from the images stored in the sourceImgs folder. The inputs in this script are case-sensitive. NOTE: HEIC files are not supported by ffmpeg.\n"
 
 echo -e "What is the pattern prefix for your image files?\n"
 read PATTERNINPUT
@@ -10,6 +10,10 @@ echo -e "What is the file extension for your image files?\n"
 read FILEEXTENSION
 echo $FILEEXTENSION
 
-ffmpeg -i ./sourceImgs/${PATTERNINPUT}-%d.${FILEEXTENSION} ${PATTERNINPUT}.gif
+echo -e "At what framerate (frames per second) would you like the gif to be rendered?\n"
+read FRAMERATE
+echo $FRAMERATE
+
+ffmpeg -framerate ${FRAMERATE} -i ./sourceImgs/${PATTERNINPUT}-%d.${FILEEXTENSION} ${PATTERNINPUT}.gif
 
 exit
