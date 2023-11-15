941f5b6f11abf2de80bcca7eb236fd244e607c9a
Timothy Dennis <timothyddennis@gmail.com>
2023-11-15 07:18:28 -0600

    feat: adjusted formatting for feedback from user inputs, added final feedback before closing

--- gifMaker.sh
+++ gifMaker.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo -e "This program generates an animated gif from the images stored in the sourceImgs folder. The inputs in this script are case-sensitive. NOTE: HEIC files are not supported by ffmpeg.\n"
+echo -e "\n~~~~~~~ Easy Gif Maker ~~~~~~~\n"
 
-echo -e "What is the pattern prefix for your image files?\n"
+echo -e "\nThis program generates an animated gif from the images stored in the sourceImgs folder. The inputs in this script are case-sensitive. NOTE: HEIC files are not supported by ffmpeg.\n"
+
+echo -e "\nWhat is the pattern prefix for your image files?\n"
 read PATTERNINPUT
-echo $PATTERNINPUT
+echo -e "\nYou input" $PATTERNINPUT
 
-echo -e "What is the file extension for your image files?\n"
+echo -e "\nWhat is the file extension for your image files?\n"
 read FILEEXTENSION
-echo $FILEEXTENSION
+echo -e "\nYou input" $FILEEXTENSION
 
-echo -e "At what framerate (frames per second) would you like the gif to be rendered?\n"
+echo -e "\nAt what framerate (frames per second) would you like the gif to be rendered?\n"
 read FRAMERATE
-echo $FRAMERATE
+echo -e "\nYou input" $FRAMERATE
 
 ffmpeg -framerate ${FRAMERATE} -i ./sourceImgs/${PATTERNINPUT}-%d.${FILEEXTENSION} ${PATTERNINPUT}.gif
+
+echo -e "\nCreated "$PATTERNINPUT".gif at "$FRAMERATE" frames per second.\n"
 
 exit
